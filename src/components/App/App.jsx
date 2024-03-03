735fd92f92c83e0ed85a86f09dd716f4f5bd6f61
Olya <116801530+marshennikovaolga@users.noreply.github.com>
2024-03-03 18:41:55 +0300

    .

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,10 @@ import { tw } from 'twind'
 import './App.css'
 import AnimatedLink from '../AnimatedLink'
 import { useState, useEffect } from 'react'
-import { faInstagram, faGithub, faSpotify, faTwitter, faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faGithub, faSpotify, faTwitter, faGoogle }
+from '@fortawesome/free-brands-svg-icons'
 import rightImage from '../../images/right.png'
 import bottomImage from '../../images/bottom.png'
-
 
 export default function App() {
     const [showLinks, setShowLinks] = useState(false)
@@ -41,28 +41,29 @@ export default function App() {
         }
     }, [showLinks]);
 
-
-
     return (
-        <div className={tw`font-raleway mt-10 text-center flex items-center justify-center`}>
+        <div className={tw`font-raleway mt-10 text-center flex items-center justify-center`} style={{ userSelect: 'none' }}>
             <div className={tw`text-white mx-auto max-w-screen-md`}>
-                <h1 className={tw`text-xl font-regular mb-4`} style={{ userSelect: 'none' }}>Marshennikova Olga</h1>
-                <p className={tw`text-s mb-3 opacity-70`} style={{ userSelect: 'none' }}>frontend dev</p>
-                <p className={tw`text-lg mb-3`} style={{ userSelect: 'none' }}>in Tbilisi since March 2024 🇬🇪</p>
+                <h1 className={tw`text-xl font-regular mb-4`}>Marshennikova Olga</h1>
+                <p className={tw`text-s mb-3 opacity-70`}>frontend dev</p>
+                <p className={tw`text-lg mb-3`}>based in Tbilisi since march 2024 🇬🇪</p>
                 <div className={tw`pt-5`}>
-                    <p className={tw`text-base mb-1`} style={{ userSelect: 'none' }}>Hey there! I'm Olya, glad to see you here.</p>
-                    <p className={tw`text-base mb-1`} style={{ userSelect: 'none' }}> Currently updating my portfolio and open to</p>
-                    <p className={tw`text-base mb-1`} style={{ userSelect: 'none' }}>collaborative projects. Soon, I'll be starting work</p>
-                    <p className={tw`text-base mb-1`} style={{ userSelect: 'none' }}>on <a href="https://github.com/marshennikovaolga/dear-diary" className={tw`text-blue-500 hover:underline`}>my diary </a>
+                    <p className={tw`text-base mb-1`}>Hey there! I'm Olya, glad to see you here.</p>
+                    <p className={tw`text-base mb-1`}> Currently updating my portfolio and open to</p>
+                    <p className={tw`text-base mb-1`}>collaborative projects. Soon, I'll be starting work</p>
+                    <p className={tw`text-base mb-1`}>on <a href="https://github.com/marshennikovaolga/dear-diary" className={tw`text-blue-500 hover:underline`}>my diary </a>
                         and assisting with the <a href="https://github.com/kuskusapp" className={tw`text-blue-500 hover:underline`}>kuskus app</a></p>
                 </div>
-                <p className={tw`text-lg mt-8 mb-8`} style={{ userSelect: 'none' }}>Find me on my socials&nbsp;&nbsp;
-                    <button onClick={buttonClick}>
-                        {showLinks ? (
-                            <img src={bottomImage} alt="Close" style={{ width: 'auto', height: 'auto', maxHeight: '1.2em' }} />
-                        ) : (
-                            <img src={rightImage} alt="Open" className='bounce-image' style={{ width: 'auto', height: 'auto', maxHeight: '1.2em' }} />
-                        )}
+
+                <div className={tw`mt-3`}>
+                    <p className={tw`text-base mb-2`}>rn i'm studying:</p>
+                    <p className={tw`text-base opacity-70`}>react native, next.js, graphql</p>
+                </div>
+
+                <p className={tw`text-lg mt-8 mb-8`}>Find me on my socials
+                    <button className={tw`ml-5`} onClick={buttonClick}>
+                        {showLinks ? (<img src={bottomImage} alt="Close" className={tw`w-auto h-5`} />)
+                        : (<img src={rightImage} alt="Open" className={tw`bounce-image w-auto h-5`} />)}
                     </button>
                 </p>
                 <div className={tw`text-white flex flex-wrap justify-center`}>
@@ -70,8 +71,8 @@ export default function App() {
                         <div key={index} className={`link-item ${showLinks && index < currentIndex ? 'show' : ''}`}
                             style={{ marginBottom: '10px' }}>
                             <AnimatedLink
-                                index={index}
                                 show={true}
+                                index={index}
                                 icon={link.icon}
                                 text={link.text}
                                 href={link.href}
